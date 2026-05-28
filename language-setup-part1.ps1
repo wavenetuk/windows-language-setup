8abe56d5ddc4f04930dc540b561f8edaa8fed3ea
Paul Martin <34447549+paul-e-martin@users.noreply.github.com>
2026-05-28 16:17:33 +0100

    Update language-setup-part1.ps1

--- language-setup-part1.ps1
+++ language-setup-part1.ps1
@@ -21,7 +21,7 @@ Param (
 
     [Parameter(Mandatory = $false, HelpMessage = 'Restart the virtual machine')]
     [ValidateSet('true', 'false')]
-    [string] $restart = 'true'
+    [string] $restart = 'false'
 )
 
 begin {
